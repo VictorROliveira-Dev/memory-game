c361bf44dddeb9441b6796992bb3e4f6e12a8708
VictorROliveira-Dev <victorramos123@outlook.com>
2023-07-10 11:09:00 -0300

    Criando card game

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+function CardGame() {
+    return `
+        <article class="card-game">
+            <img class="card-game-img" src="images/brain-img.png" alt="logo alura">
+        </article>
+    `;
+}
+
+export default CardGame;
