6a42f21848a407c4404625754e6d70fd7c151fce
VictorROliveira-Dev <victorramos123@outlook.com>
2023-07-11 11:20:27 -0300

    Adding rotation to card

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,10 +1,15 @@
 import CardGame from "../CardGame";
+import './style.css';
 
 function CardFrontBack() {
     return /*html*/`
         <article class="card-front-back">
-            ${CardGame()}
-            ${CardGame("panda-dance-removebg", "panda dançando")}
+            <div class="card -front">
+                ${CardGame()}
+            </div>
+            <div class="card -back">
+                ${CardGame("panda-dance-removebg", "panda dançando")}
+            </div>
         </article>
     `
 }
